39e47e874a66951dfbd228dd54586fdae6f57bd1
silvio.wangler <silvio.wangler@36b4d433-4d3f-0410-96d9-f32f2e8bbd28>
2009-12-28 15:13:58 +0000

    added mail-plugin

--- trunk/grails-app/conf/Config.groovy
+++ trunk/grails-app/conf/Config.groovy
@@ -6,46 +6,92 @@
 //                             "file:${userHome}/.grails/${appName}-config.properties",
 //                             "file:${userHome}/.grails/${appName}-config.groovy"]
 
-if(System.properties["${appName}.config.location"]) {
-    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
-}
+// if(System.properties["${appName}.config.location"]) {
+//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
+// }
+grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
+grails.mime.use.accept.header = false
+grails.mime.types = [html: ['text/html', 'application/xhtml+xml'],
+   xml: ['text/xml', 'application/xml'],
+   text: 'text/plain',
+   js: 'text/javascript',
+   rss: 'application/rss+xml',
+   atom: 'application/atom+xml',
+   css: 'text/css',
+   csv: 'text/csv',
+   all: '*/*',
+   json: ['application/json', 'text/json'],
+   form: 'application/x-www-form-urlencoded',
+   multipartForm: 'multipart/form-data'
+]
+// The default codec used to encode data with ${}
+grails.views.default.codec = "none" // none, html, base64
+grails.views.gsp.encoding = "UTF-8"
+grails.converters.encoding = "UTF-8"
+// enable Sitemesh preprocessing of GSP pages
+grails.views.gsp.sitemesh.preprocess = true
+// scaffolding templates configuration
+grails.scaffolding.templates.domainSuffix = 'Instance'
+
+// Set to false to use the new Grails 1.2 JSONBuilder in the render method
+grails.json.legacy.builder = false
 // enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = true
+// whether to install the java.util.logging bridge for sl4j. Disable fo AppEngine!
+grails.logging.jul.usebridge = true
+// packages to include in Spring bean scanning
+grails.spring.bean.packages = []
 
-// log4j configuration
-log4j {
-    appender.stdout = "org.apache.log4j.ConsoleAppender"
-	appender.'stdout.layout'="org.apache.log4j.PatternLayout"
- 	appender.'stdout.layout.ConversionPattern'='[%r] %c{2} %m%n'
-    rootLogger="error,stdout"
-    logger {
-        grails="debug,stdout"
-        org {
-            codehaus.groovy.grails.web.servlet="error,stdout"  //  controllers
-            codehaus.groovy.grails.web.errors="error,stdout"  //  web layer errors            
-			codehaus.groovy.grails.web.pages="error,stdout" //  GSP
-        	codehaus.groovy.grails.web.sitemesh="error,stdout" //  layouts
-        	codehaus.groovy.grails."web.mapping.filter"="error,stdout" // URL mapping
-        	codehaus.groovy.grails."web.mapping"="error,stdout" // URL mapping
-            codehaus.groovy.grails.commons="info,stdout" // core / classloading
-            codehaus.groovy.grails.plugins="error,stdout" // plugins
-            codehaus.groovy.grails.orm.hibernate="error,stdout" // hibernate integration
-            springframework="off,stdout"
-            hibernate="off,stdout"
-        }
-    }
-	additivity.'default' = false
-    additivity {
-		grails=false
-		org {
-           codehaus.groovy.grails=false
-           springframework=false
-		   hibernate=false
-		}
-    }
+// set per-environment serverURL stem for creating absolute links
+environments {
+   production {
+      grails.serverURL = "http://www.sonja-matt?hias.ch/${appName}"
+   }
+   development {
+      grails.serverURL = "http://localhost:8080/${appName}"
+   }
+   test {
+      grails.serverURL = "http://localhost:8080/${appName}"
+   }
+
 }
 
+// log4j configuration
+log4j = {
+   // Example of changing the log pattern for the default console
+   // appender:
+   //
+   //appenders {
+   //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+   //}
 
-// The following properties have been added by the Upgrade process...
-grails.views.default.codec="none" // none, html, base64
-grails.views.gsp.encoding="UTF-8"
+
+   error 'org.codehaus.groovy.grails.web.servlet',  //  controllers'org.codehaus.groovy.grails.web.pages', //  GSP
+      'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+      'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+      'org.codehaus.groovy.grails.web.mapping', // URL mapping
+      'org.codehaus.groovy.grails.commons', // core / classloading
+      'org.codehaus.groovy.grails.plugins', // plugins
+      'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+      'org.springframework',
+      'org.hibernate',
+      'net.sf.ehcache.hibernate'
+
+   warn 'org.mortbay.log'
+}
+
+grails {
+   mail {
+      host = "smtp.gmail.com"
+      port = 465
+      username = "silvio.wangler@gmail.com"
+      password = "*One+minute*"
+      props = ["mail.smtp.auth": "true",
+         "mail.smtp.socketFactory.port": "465",
+         "mail.smtp.socketFactory.class": "javax.net.ssl.SSLSocketFactory",
+         "mail.smtp.socketFactory.fallback": "false"]
+   }
+}
+
+grails.mail.default.from = "saw@silviowangler.ch"
+
